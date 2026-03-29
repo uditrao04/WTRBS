d207e412b8b2e06d376b90dd73010690656e62c6
uditrao04 <uditrao04@gmail.com>
2026-03-30 04:10:37 +0500

    Updated blockMesh, snappyHexMesh, makeBlade

--- case/makeBlades.sh
+++ case/makeBlades.sh
@@ -1,39 +1,88 @@
-#!/bin/bash
 # This file creates new files after transforming them into new positions/angles. This is denoted by prefixes and suffixes.
 # 'O' at suffix means 'At Origin'.
 # 'P' at suffix means 'Pitch Angle Applied"
 # '1' at prefix means 'The 1st Blade of the wind turbine'.
+
+# To use this type:
+#	./makeBlades.sh {Hub Radius} {Pitch Angle} {Number of Blades}
+
+# By default:
+#	Hub Radius = 10
+#	Pitch Angle = 45
+#	Number of Blades = 3
+
+
+
+#!/bin/bash
+
 # This is a bash safety setting saying, "If any command fails, stop the script immediately"
 set -e 
 
 TS="constant/triSurface"
+LOG="log.makeBlades"
 
-# Hub Radius
-HR=10
+HR=${1:-10}						# Hub Radius, 1st Command-Line Argument
+PA=${2:-45}						# Pitch angle
+NB=${3:-3}						# Number of Blades
 
-# Pitch angle
-PA=45
+if [ "$NB" -lt 1 ]		# Input validation
+then
+	echo "Error: Number of Blades (NB) must be atleast 1"
+	exit 1
+fi
+
+STEP=$(awk "BEGIN {print 360/$NB}")			# Blade Step Angle
+
+
+: > "$LOG"		# Clear the old log file			
+runCmd()		# Logging function (Wrapper Function)
+{
+	echo ">>> $*" >> "$LOG"		# Essentially ">>> surfaceTransformPoints >> "$LOG"
+	"$@" >> "$LOG" 2>&1		# Both stdout and stderr are redirected to the log file.
+}
+
+echo "Hub Radius = $HR"
+echo "Pitch Angle = $PA"
+echo "Blade Count = $NB"
+echo "Step Angle = $STEP"
+echo "Detailed Output -> $LOG"
 
 # Hub at Origin
-surfaceTransformPoints \
-    -translate "(-$HR -$HR -$HR)" \
-    "$TS/hub.stl" \
-    "$TS/hubO.stl"
-    
-# Blade 1 at Origin 
-surfaceTransformPoints \
-    -translate "(0 $HR 0)" \
-    "$TS/bladeO.stl" \
-    "$TS/1bladeO.stl"
+runCmd surfaceTransformPoints \
+	-translate "(-$HR -$HR -$HR)" \
+	"$TS/hub.stl" \
+	"$TS/hubO.stl"
 
-surfaceTransformPoints \
-    -rotate-x 120 \
-    "$TS/blade1.stl" \
-    "$TS/blade2.stl"
+# Base Blade at Origin
+runCmd surfaceTransformPoints \
+	-translate "(-0.5 0 -10)" \
+	"$TS/blade.stl" \
+	"$TS/bladeO.stl" 
+   
+# Blade 1 at Position 
+# A little bit inside the hub to prevent meshing complications
+runCmd surfaceTransformPoints \
+	-translate "(0 5 0)" \
+	"$TS/bladeO.stl" \
+	"$TS/1bladeO.stl"
 
-surfaceTransformPoints \
-    -rotate-x 240 \
-    "$TS/blade1.stl" \
-    "$TS/blade3.stl"
+# Blade 1 at Origin and Pitched
+runCmd surfaceTransformPoints \
+	-rotate-y $PA \
+	"$TS/1bladeO.stl" \
+	"$TS/1bladeOP.stl"
+
+for((i=2;i<=NB;i++))
+do
+	# Basically angle for each blade ('N'bladeOP) = N * Blade Step Angle
+	ANGLE=$(awk "BEGIN {print($i-1)*$STEP}")
+	
+	# Creation of remaining blades
+	runCmd surfaceTransformPoints \
+    		-rotate-x "$ANGLE" \
+    		"$TS/1bladeOP.stl" \
+    		"$TS/${i}bladeOP.stl"
+done
 
 echo "Done"
+echo "Created $NB blades."
